cb1c50c9514622be5965a855ce1b6467bbadcb47
SyifaulM <syifa.ulm01@gmail.com>
2023-05-25 09:55:52 +0700

    finished project

---
+++ app/src/main/java/com/example/utspraktik_if9_10120768_syifaulmukminin/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.utspraktik_if9_10120768_syifaulmukminin
+
+// Tanggal Pengerjaan : 24 Mei 2023
+// NIM  : 10120768
+// Nama : Syifaul Mukminin
+// Kelas: IF9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
